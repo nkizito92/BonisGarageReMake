2556d72f9f5443de8a8d34d652ddc808833e8627
nkizito92 <nkizito57@gmail.com>
2021-09-14 22:20:45 -0500

    Setting up bonis-garage web app

---
+++ bonis-garage/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import './index.css';
+import App from './App';
+import combineReducers from './reducers/index';
+import reportWebVitals from './reportWebVitals';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(combineReducers, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
