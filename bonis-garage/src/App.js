79192541b8ec07f834093bf89254a2f9e97015b7
nkizito92 <nkizito57@gmail.com>
2021-09-22 22:30:05 -0500

    imported the component Home and Navbar

--- bonis-garage/src/App.js
+++ bonis-garage/src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
 import { Route, Switch } from 'react-router-dom'
+import Navbar from './headers/Navbar'
+import Home from './components/Home'
 import './App.css';
+import './css/style.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+        <Navbar />
+      </nav>
       <main>
-        <div>
-          Hi My name is Boni I am the founder of Boni's Garage.
-        </div>
+        <Switch>
+          <Route exact path="/" component={() => <Home />} />
+        </Switch>
       </main>
+      <footer>
+        Copy Right
+      </footer>
     </div>
   );
 }
