f0a4b1b5253248079dce342d85f3bdda4a575950
nkizito92 <nkizito57@gmail.com>
2021-09-23 22:39:03 -0500

    added the component Home

---
+++ bonis-garage/src/components/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <>
+            <div className="imageSlide">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Home
